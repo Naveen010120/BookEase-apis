68443f15927cae71f60a4777ae6c3a170b4d1868
NaveenSingh <raghuvamshinaveensingh@gmail.com>
2025-02-10 15:13:54 +0530

    thrid commit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function getUserDataFromReq(req){
 
 app.use(cors({
     credentials: true,
-    origin: 'https://book-ease-project.vercel.app/',
+    origin: 'https://book-ease-project.vercel.app',
 }))
 app.get('/test', (req, res) => {
     // console.log(process.env.MONGO_URL)
