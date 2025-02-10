f26b6986e6d2f4cc6911c95a6d890aee776c966e
NaveenSingh <raghuvamshinaveensingh@gmail.com>
2025-02-10 16:22:13 +0530

    changing path

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function getUserDataFromReq(req){
 
 app.use(cors({
     credentials: true,
-    origin: 'https://book-ease-project.vercel.app',
+    origin: 'https://book-ease-project-git-main-naveen-singhs-projects-4cb555d5.vercel.app',
 }))
 app.get('/test', (req, res) => {
     // console.log(process.env.MONGO_URL)
